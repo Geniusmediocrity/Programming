f17e3cc31f97d20fd0088c008892d9b8b8ff709c
Alexey <lesal2767@gmail.com>
2025-02-05 09:29:16 +0300

    to ubunyu

--- BOT/main.py
+++ BOT/main.py
@@ -1,7 +1,16 @@
-import openai
-from aiogram import Bot, types
-from aiogram.dispatcher import dispatcher
-# для того чтобы бот запускался постоянно
-
+from aiogram import Bot, Dispatcher, executor, types
 from tokn import *
 
+bot = Bot(TELEGRAM_TOKEN)
+dp = Dispatcher()
+START_MESSAGE = "СоСал!"
+
+@dp.message_handler(comannds=["start"])
+async def start_message(message: type.Message) -> type.Message:
+    await message.reply(START_MESSAGE)
+        
+@dp.message_handler()
+async def send_message(message: type.Message) -> type.Message:
+    await message.reply()
+
+start_message()
