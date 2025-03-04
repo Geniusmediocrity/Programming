d221a3827d8c92632c432cadc99e6778585e5953
alexy <lesal2767@gmail.com>
2025-03-04 19:34:03 +0300

    English DONE

--- Python/learning_filemanagment/OS_lib_2.py
+++ Python/learning_filemanagment/OS_lib_2.py
@@ -7,16 +7,24 @@ import time
 
 os.mkdir("test_dir")
 os.mknod("test.txt")
-time.sleep(1)
 os.replace("test.txt", "./test_dir/test.txt")
-print("file removing succesful")
-shutil.rmtree("test_dir")
+print(f"\rreplace...", end="")
+time.sleep(2)
+print("\rfile replacing succesful")
 
+print(f"\rremoving...", end="")
+time.sleep(2)
+shutil.rmtree("test_dir")   #? удаление не пустой директории
+print("\rtree removing succesful")
+
+print("-" * 40)
 
 os.mkdir("Test_dir")
-os.chdir("./Test_dir")
-os.mknod("Test_file.txt")
-os.chdir("..")
+print(f"\rmaking node...", end="")
+time.sleep(2)
+os.mknod("./Test_dir/Test_file.txt")
+print("\rnode making succesfull")
+print(f"\rremoving...", end="")
 time.sleep(2)
 shutil.rmtree("Test_dir")   #? удаление не пустой директории
-
+print("\rtree removing succesful")
