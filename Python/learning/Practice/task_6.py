1338afdaa3620a84acb31a3924b6892032710385
Alexey <lesal2767@gmail.com>
2025-03-05 19:56:09 +0300

    Done commit

--- Python/learning/Practice/task_6.py
+++ Python/learning/Practice/task_6.py
@@ -30,3 +30,33 @@ def main(path: str, encoding="utf-8"):
 result = main(path="Practice/Задание 6.txt")
 print(result)
 #? среднее время выполнения программы: 1.79725с
+
+# ----------------------------------------------------------------------------------
+
+def max_len(seq: str|list|tuple) -> int:
+    start = 0
+    result = []
+    last_symb = seq[0]
+    counter = 0
+    while start < len(seq) - 1:
+        if seq[start: start + 2] in "AB CB":
+            counter += 1
+            start += 2
+        else:
+            start += 1
+            result.append(counter)
+            counter = 0
+    return max(result)
+        
+        
+
+def main(path: str, encoding="utf-8"):
+    with open(file=path, mode="rt", encoding=encoding) as file:
+        read_file = file.read()
+        return max_len(read_file)
+
+
+result = main(path="Practice/Задание 6.txt")
+print(result)
+#? Среднее время выполнения программы: 0.9849000000000001с что в 2 раза быстрее(если кортеж)
+#? Если строка: ~0.8989889328с, что на 10 секунд меньше по времени выполнения
